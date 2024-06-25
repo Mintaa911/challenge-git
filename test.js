af4c700b6560c8fa0ecfd41dac9687c67478eca7
Mintesnot <mintesnotbezabhi99@gmail.com>
2024-06-25 11:52:09 +0300

    feat: add user-agent endpoint

--- test.js
+++ test.js
@@ -5,12 +5,22 @@ const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
 const server = require('./server')
 
 const urlBase = `http://localhost:${PORT}`
+const urlUserAgent = `http://localhost:${PORT}/user-agent`
 
 tape('should respond hello', (t) => {
   jsonist.get(urlBase, (err, body) => {
     if (err) t.error(err)
 
     t.equal(body.msg, 'hello')
+    t.end()
+  })
+})
+
+tape('should respond user agent', (t) => {
+  jsonist.get(urlUserAgent, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.msg, 'User agent')
     t.end()
   })
 })
